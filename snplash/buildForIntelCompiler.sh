84ee7659306ea9b8942db2a920b44e86f99d94df
Richard Guy <guyrt7@bc103bl13.deac.wfu.edu>
2012-02-06 23:04:44 -0500

    CMake working with intel compiler via special shell script.

---
+++ snplash/buildForIntelCompiler.sh
@@ -0,0 +1,7 @@
+mkdir build
+cd build
+cmake -D CMAKE_CXX_COMPILER=/opt/intel/cce/9.1.049/bin/icpc -D ICPC=TRUE ..
+
+echo "Now cd into build and type make. The executable will be in this directory."
+
+
