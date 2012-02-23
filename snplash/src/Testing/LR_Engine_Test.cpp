8315e3ab38ee5765a13ff51b022d2e4b433aef36
Richard T. Guy <richardtguy84@gmail.com>
2012-02-23 17:26:29 +0100

    Messaging and a second test

--- snplash/src/Testing/LR_Engine_Test.cpp
+++ snplash/src/Testing/LR_Engine_Test.cpp
@@ -45,13 +45,51 @@ class LR_Engine_Test : public ::testing::Test {
 
 };
 
+class LR_Separable_Test : public ::testing::Test{
 
+	protected: 
+
+	vector<vector<double> > cov;
+        vector<double> response;
+
+	void SetUp(){
+
+        for (int i=0;i < 4; i++)
+                response.push_back(1.0);
+        for (int i=0;i < 4; i++)
+                response.push_back(0.0);
+                
+        for (int i=0; i < 2; i++)
+                cov.push_back( vector<double>() );
+        for (int i=0; i < 4; i++){
+                cov[0].push_back(1.0);
+        }
+        for (int i=0; i < 4; i++){
+                cov[0].push_back(2.0);
+        }
+        
+        for (int i=0; i < 8; i++){
+                cov[1].push_back(i);
+        }
+	}
+
+}; 
+
+//--------------------------------------------------------------------------        
 
 TEST_F(LR_Engine_Test, test_newton_raphson) {
   
 	vector<vector<double> > invInfMatrix = vecops::getDblVec(inMat.size(), inMat.size());
 	vector<double> betas = lr.newtonRaphson(inMat, phenotype, invInfMatrix);
 	ASSERT_DOUBLE_EQ(betas[0], 0.36750847348763349);
+	ASSERT_DOUBLE_EQ(betas[1], 1.4467808518431764);
+	ASSERT_DOUBLE_EQ(betas[2], -1.5618476151485159);
 }
 
+TEST_F(LR_Separable_Test, test_separable) {
+	
+	LogisticRegression lr;
+        int separableColumn = lr.dataIsSeparable(cov, response);
+        ASSERT_EQ( separableColumn, 0);  
 
+}
