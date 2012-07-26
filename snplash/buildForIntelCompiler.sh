8f5b90089146296a0200d0f6d584a1e69047077f
David McWilliams <dmcwilli@rhel6head1.deac.wfu.edu>
2012-07-26 09:46:23 -0400

    Replaced explicit path to icpc with a call to 'which'.

--- snplash/buildForIntelCompiler.sh
+++ snplash/buildForIntelCompiler.sh
@@ -1,6 +1,9 @@
 mkdir build
 cd build
-cmake -D CMAKE_CXX_COMPILER=/opt/intel/cce/9.1.049/bin/icpc -D ICPC=TRUE ..
+
+ICPC=`which icpc`
+
+cmake -D CMAKE_CXX_COMPILER=$ICPC -D ICPC=TRUE ..
 
 echo "Now cd into build and type make. The executable will be in this directory."
 
