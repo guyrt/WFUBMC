bb4546d599c5127db2618d924ac2088649ea4356
David R. McWilliams <dmcwilli@wakehealth.edu>
2013-01-22 09:55:08 -0500

    Modify par.glm.cov.R to include the snp recoded as the additive model as a covariate in the lack-of-fit regression.

--- snplash/t/Rcode/par.glm.cov.R
+++ snplash/t/Rcode/par.glm.cov.R
@@ -10,44 +10,59 @@
 # 15-Dec-2010 Initiate project
 # 20-Dec-2010 Pass in model as a parameter
 # 09-Jun-2011 Make parallel; adapted from multi.lm
+# 22-Jan-2013 Include the additive model for a snp as a covariate
+#             in the lack-of-fit calculation.
 
-# par.glm.cov
-#
 # Accepts: A dataframe containing a response and snp data in biallelic
 #          format, the response column index, and the index of the
 #          start column for the snp data, which must be in one
 #          contiguous block.
 #
-# Returns: A dataframe with the coefficient information provided by (g)lm
+# Returns: A dataframe with the coefficient information provided by glm
 #
+# Requires: The doMC package must be already loaded.
 
-# Need test to see if package doMC is already loaded
+# ToDo: Test for package doMC and fail gracefully if absent
 
 par.glm.cov <- function (data.df, qvar, snp.start, gen.model, cov1, cov2) {
   d <- data.df
   n.col <- ncol(d)
   n.snp <- (n.col-snp.start+1)/2
 
+  is.lof <- (gen.model=="lof")
+  
   snp.num <- 1
 
   mcols <- 4*4+2
+  if(is.lof) {
+    mcols <- mcols+4
+  }
   results.m <- matrix(nrow=n.snp, ncol=mcols)
   
   results.m <-foreach(i=seq(snp.start, n.col-1, 2), .combine=rbind) %dopar% {
     nz <- d[,i]!=0 & d[,i+1]!=0
     snp <- recode(d[nz,i], d[nz,i+1], gen.model)
 
+    if(is.lof) {
+      snp.add <- recode(d[nz,i], d[nz,i+1], "add")
+    }
+
     if (all(is.na(snp))) {
       rep(NA, mcols)
     } else {
-      int.res <- rep(NA,4)
-      snp.res <- rep(NA,4)
-      cv1.res <- rep(NA,4)
-      cv2.res <- rep(NA,4)
-      aic.res <- NA
-      dev.res <- NA
+      int.res     <- rep(NA,4)
+      snp.res     <- rep(NA,4)
+      cv1.res     <- rep(NA,4)
+      cv2.res     <- rep(NA,4)
+      snp.add.res <- rep(NA,4)
+      aic.res     <- NA
+      dev.res     <- NA
 
-      this.glm <- glm(qvar[nz] ~ snp + cov1[nz] + cov2[nz], family="binomial")
+      if(is.lof) {
+        this.glm <- glm(qvar[nz] ~ snp + cov1[nz] + cov2[nz] + snp.add, family="binomial")
+      } else {
+        this.glm <- glm(qvar[nz] ~ snp + cov1[nz] + cov2[nz], family="binomial")
+      }
       this.coef <- summary(this.glm)$coefficients
       snp.num <- snp.num + 1
 
@@ -56,7 +71,7 @@ par.glm.cov <- function (data.df, qvar, snp.start, gen.model, cov1, cov2) {
         if(length(grep("Int", nms[i], value=T)) > 0) {
           int.res <- this.coef[i,]
         }
-        if(length(grep("snp", nms[i], value=T)) > 0) {
+        if(length(grep("^snp$", nms[i], value=T)) > 0) {
           snp.res <- this.coef[i,]
         }
         if(length(grep("cov1", nms[i], value=T)) > 0) {
@@ -65,22 +80,37 @@ par.glm.cov <- function (data.df, qvar, snp.start, gen.model, cov1, cov2) {
         if(length(grep("cov2", nms[i], value=T)) > 0) {
           cv2.res <- this.coef[i,]
         }
+        if(length(grep("snp.add", nms[i], value=T)) > 0) {
+          snp.add.res <- this.coef[i,]
+        }
+      }
+
+      out.row <- c(int.res, snp.res, cv1.res, cv2.res)
+      if(is.lof) {
+        out.row <- append(out.row, snp.add.res)
       }
       aic.res <- this.glm$aic
       dev.res <- this.glm$deviance
 
-      c(int.res, snp.res, cv1.res, cv2.res, aic.res, dev.res)
+      out.row <- append(out.row, aic.res)
+      out.row <- append(out.row, dev.res)
     }
   } # end foreach(i in snp)
-#  snp.num <- snp.num -1
 
   results.df <- data.frame(results.m)
   row.names(results.df) <- NULL
 
-  names(results.df) <- c("b0", "b0.se", "b0.z", "b0.p",
-                         "b1", "b1.se", "b1.z", "b1.p",
-                         "c1", "c1.se", "c1.z", "c1.p",
-                         "c2", "c2.se", "c2.z", "c2.p",
-                         "aic", "null.dev")
+  var.names <- c("b0", "b0.se", "b0.z", "b0.p",
+                 "b1", "b1.se", "b1.z", "b1.p",
+                 "c1", "c1.se", "c1.z", "c1.p",
+                 "c2", "c2.se", "c2.z", "c2.p")
+
+  if(is.lof) {
+    var.names <- append(var.names,
+                        c("snp.add", "snp.add.se", "snp.add.z", "snp.add.p"))
+  }
+  var.names <- append(var.names, c("aic", "null.dev"))
+  names(results.df) <- var.names
+  
   results.df
 } # end main
